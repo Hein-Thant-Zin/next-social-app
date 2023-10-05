15a436067a06e2c07f83e5fb55b33f8214dd05cd
heinthantzin <heintz414@gmail.com>
2023-10-05 13:59:28 +0630

    add_redirect

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,16 +1,17 @@
-// 'use client'
+'use client'
 import { getServerSession } from "next-auth";
 import UserProfile from "./UserProfile";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from 'next/navigation';
+import { useSession } from "next-auth/react";
 
-export default async function UserDashboard() {
+export default function UserDashboard() {
    
-
+const { data: session, status } = useSession(authOptions);
   // console.log({res});
-  // if (!session) {
-  //   redirect ('api/auth/signin')
-  // }
+  if (!session) {
+    redirect ('api/auth/signin')
+  }
   return (
     <section>
       <div className="container mx-auto">
