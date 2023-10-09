c5a5cd595bef14cae2b0368e014805b4404d5890
heinthantzin <heintz414@gmail.com>
2023-10-09 22:21:36 +0630

    useTransition

--- components/FollowButton/FollowClient.jsx
+++ components/FollowButton/FollowClient.jsx
@@ -1,10 +1,12 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
+import React, { useState, useTransition } from 'react'
 
 export default function FollowClient({ isFollowing, targetUserId }) {
     const router = useRouter();
     const [isFetching, setIsFetching] = useState(false);
+    const [isPending, startTransition] = useTransition();
+    const isMutating = isFetching || isPending;
 
     async function follow() {
         setIsFetching(true);
@@ -18,10 +20,14 @@ export default function FollowClient({ isFollowing, targetUserId }) {
        });
     //    console.log(res);
 
-       setIsFetching(false);
+        setIsFetching(false);
+        
+        startTransition(() => {
+            //only rendering backend state without interferencing frontend
+       router.refresh();    
+        })
 
-       //only rendering backend state without interferencing frontend
-       router.refresh();
+       
     }
     async function unfollow() {
         setIsFetching(true);
@@ -31,11 +37,14 @@ export default function FollowClient({ isFollowing, targetUserId }) {
         })
 
         setIsFetching(false);
-        router.refresh();
+        startTransition(() => {
+            //only rendering backend state without interferencing frontend
+       router.refresh();    
+        }) 
     }
   if (isFollowing) {
-        return <button className='follow-btn' onClick={unfollow}>{ isFetching ? '...' : 'Unfollow' }</button>
+        return <button className='transition follow-btn hover:bg-slate-700' onClick={unfollow}>{ isMutating ? '...' : 'Unfollow' }</button>
     } else {
-      return <button className='follow-btn' onClick={follow}>{ isFetching ? '...' : 'Follow' }</button>
+      return <button className='transition follow-btn hover:bg-slate-700' onClick={follow}>{ isMutating ? '...' : 'Follow' }</button>
     }
 }
