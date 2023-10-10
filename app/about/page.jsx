468ecf9e77d6d7930a8837ba503623a0640a5749
heinthantzin <heintz414@gmail.com>
2023-10-10 10:03:14 +0630

    follow check

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,8 +2,8 @@ export const dyanamic = 'force-static';
 
 export default function About() {
   return (
-    <main>
-      <h1>About Us</h1>
+    <main className="container p-4 mx-auto mt-4 text-center">
+      <h1 className="" >About Us</h1>
       <p>A positive social media plaform</p>
     </main>
   )
--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,9 +1,19 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import FollowButton from '@/components/FollowButton/FollowButton';
 import { prisma } from '@/lib/prisma'
+import { getServerSession } from 'next-auth';
 import Image from 'next/image';
 import React from 'react'
 
 export default async function UserDetails({ params }) {
+  const session = await getServerSession(authOptions);
+  const currentUserEmail = session?.user?.email;
+  const currentUserId = await prisma.user.findUnique({
+      where: {
+        email: currentUserEmail,
+      },
+    })
+          .then((user) => user.id);
   const user = await prisma.user.findUnique({
     where: {
       id: params.id,
@@ -12,16 +22,14 @@ export default async function UserDetails({ params }) {
 
   const {id,name,image,age,bio} = user ?? {}
 
-
   return (
-    <div className='max-w-lg p-3 mx-auto text-center bg-green-50'>
+    <main className='max-w-lg p-4 mx-auto mt-6 text-center shadow bg-slate-100'>
          <div className='font-semibold'> User Details</div>
-      <h2>{name}</h2>
     <Image className='mx-auto mt-2 rounded-sm aspect-square ' src={image}  width={80} height={80} alt={name} />
+      <h2 className='mt-2'>{name}</h2>
       <h3>{ bio }</h3>
-      <p>{age} years</p>
-
-     <FollowButton targetUserId={id} />
-    </div>
+      <p>{age?`${age} years` : ''} </p>
+      {currentUserId !== id  ?  <FollowButton targetUserId={id} /> : ''}
+    </main>
   )
 }
