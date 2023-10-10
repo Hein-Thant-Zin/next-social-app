ddaa3b468a1ecf20e41f9900dc4c0aa0327f3c01
heinthantzin <heintz414@gmail.com>
2023-10-10 22:00:05 +0630

    add user bio

---
+++ app/dashboard/UserBio.jsx
@@ -0,0 +1,10 @@
+export default async function UserBio() {
+    const user = await prisma.user.findUnique({
+        where: {
+        email: currentUserEmail,
+      },
+    });
+  const userBio = user.bio;
+  // console.log(userBio);
+    return userBio;
+}
--- app/dashboard/UserProfile.jsx
+++ app/dashboard/UserProfile.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { authOptions } from '../api/auth/[...nextauth]/route';
 import { useSession } from 'next-auth/react';
+import UserBio from './UserBio';
 
 export default  function UserProfile() {
     const { data: session, status } = useSession(authOptions);
@@ -33,7 +34,6 @@ export default  function UserProfile() {
   };
   if (status === 'loading') return null;
   const user = session.user;
-  // const usr = await prisma.user.findMany();
   return (
       <div className='max-w-xl p-5 mx-auto mt-8 border rounded-md shadow'>
           <h2 className='my-1 text-lg font-semibold text-center'>Update your profile</h2>
@@ -44,7 +44,7 @@ export default  function UserProfile() {
               </div>
                 <div className='mt-6'>
                    <label className='block' htmlFor="bio">Bio</label>
-                  <textarea className='w-full p-2 pt-1 border rounded-md shadow-sm focus:border-slate-400 outline-0'  name='bio' id="bio" defaultValue={user?.bio ?? ''} />
+                  <textarea className='w-full p-2 pt-1 border rounded-md shadow-sm focus:border-slate-400 outline-0'  name='bio' id="bio" /><UserBio />
               </div>
               <button  className='px-4 mt-4 rounded shadow bg-slate-300' type='submit'>Save</button>
       </form>
