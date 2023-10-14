06a469965f95d79d5079e557e1f91624c174ef48
heinthantzin <heintz414@gmail.com>
2023-10-14 16:12:50 +0630

    fix

--- components/SlideAnimation.jsx
+++ components/SlideAnimation.jsx
@@ -12,7 +12,7 @@ export default function SlideAnimation({ posts }) {
     return (
         <ul   className='max-w-3xl mx-auto mt-2 space-y-4'>
             {posts.map((post,index) => (
-                <motion.li layoutId={posts.id} initial='hidden' variants={itemVarients} key={posts.id} transition={{ duration : 1.5 ,delay : (index + 1)*0.5}} animate='visible'  className='p-2 text-center transition border rounded shadow hover:bg-slate-100' key={post.slug}>
+                <motion.li layoutId={posts.id} initial='hidden' variants={itemVarients} transition={{ duration : 1.5 ,delay : (index + 1)*0.5}} animate='visible'  className='p-2 text-center transition border rounded shadow hover:bg-slate-100' key={post.slug}>
                   <Link  href={`/posts/${post.slug}`}><ul className=''>{post.title}</ul>
                 </Link>  
       </motion.li>
