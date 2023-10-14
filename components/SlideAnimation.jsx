f668f29d93ca2ebbe12b261da9134e7cd9ce4123
heinthantzin <heintz414@gmail.com>
2023-10-14 16:10:30 +0630

    smooth transition

--- components/SlideAnimation.jsx
+++ components/SlideAnimation.jsx
@@ -5,18 +5,18 @@ import {motion} from 'framer-motion'
 
 export default function SlideAnimation({ posts }) {
     const itemVarients = {
-        hidden: { opacity: 0, x: -200 },
-        visible: { opacity: 1, x: 0 },
+        hidden: { opacity: 0 },
+        visible:  (customDelay)=>( { opacity: 1, x: 0 ,duration : 1.5 ,delay : customDelay}),
         // transition : { duration : 1 }
     }
     return (
-      <motion.ul variants={itemVarients} transition={{ duration : 1.5 }} animate='visible' initial='hidden' className='max-w-3xl mx-auto mt-2 space-y-4'>
-            {posts.map((post) => (
-                <motion.article  className='p-2 text-center transition border rounded shadow hover:bg-slate-100' key={post.slug}>
-                  <Link  href={`/posts/${post.slug}`}><h2 className=''>{post.title}</h2>
+        <ul   className='max-w-3xl mx-auto mt-2 space-y-4'>
+            {posts.map((post,index) => (
+                <motion.li layoutId={posts.id} initial='hidden' variants={itemVarients} key={posts.id} transition={{ duration : 1.5 ,delay : (index + 1)*0.5}} animate='visible'  className='p-2 text-center transition border rounded shadow hover:bg-slate-100' key={post.slug}>
+                  <Link  href={`/posts/${post.slug}`}><ul className=''>{post.title}</ul>
                 </Link>  
-      </motion.article>
+      </motion.li>
       ))}
-        </motion.ul>
+        </ul>
   )
 }
