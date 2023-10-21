cbc3235f594811c51a51c38e4e7764304c5916c2
heinthantzin <heintz414@gmail.com>
2023-10-21 16:20:11 +0630

    change spin style

--- app/users/loading.jsx
+++ app/users/loading.jsx
@@ -1,15 +1,14 @@
-import { Loader2 } from "lucide-react"
+import { Loader2 } from "lucide-react";
 
-import { Button } from "@/components/ui/button"
-
-
-
+import { Button } from "@/components/ui/button";
 
 export default function Loading() {
   return (
-   <Button className='text-center' disabled>
-      <Loader2 className="w-4 h-4 mr-2 animate-spin" />
-      Please wait
-    </Button>
-  )
+    <div className="mt-10 text-center">
+      <Button className="" disabled>
+        <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+        Please wait
+      </Button>
+    </div>
+  );
 }
