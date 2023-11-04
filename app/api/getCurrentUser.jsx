3da620719f1f752a9f181b614f412782da3c50ae
heinthantzin <heintz414@gmail.com>
2023-11-04 16:43:32 +0630

    final

--- app/api/getCurrentUser.jsx
+++ app/api/getCurrentUser.jsx
@@ -1,16 +1,16 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./auth/[...nextauth]/route";
-import { prisma } from "@/lib/prisma";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "./auth/[...nextauth]/route";
+// import { prisma } from "@/lib/prisma";
 
-export default async function getCurrentUser() {
-  const session = await getServerSession(authOptions);
-  const currentUserEmail = session?.user?.email;
-  const currentUserId = await prisma.user
-    .findUnique({
-      where: {
-        email: currentUserEmail,
-      },
-    })
-    .then((user) => user.id);
-  return currentUserId;
-}
+// export default async function getCurrentUser() {
+//   const session = await getServerSession(authOptions);
+//   const currentUserEmail = session?.user?.email;
+//   const currentUserId = await prisma.user
+//     .findUnique({
+//       where: {
+//         email: currentUserEmail,
+//       },
+//     })
+//     .then((user) => user.id);
+//   return currentUserId;
+// }
--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,35 +1,42 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import FollowButton from '@/components/FollowButton/FollowButton';
-import { prisma } from '@/lib/prisma'
-import { getServerSession } from 'next-auth';
-import Image from 'next/image';
-import React from 'react'
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import FollowButton from "@/components/FollowButton/FollowButton";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+import React from "react";
 
 export default async function UserDetails({ params }) {
   const session = await getServerSession(authOptions);
   const currentUserEmail = session?.user?.email;
-  const currentUserId = await prisma.user.findUnique({
+  const currentUserId = await prisma.user
+    .findUnique({
       where: {
         email: currentUserEmail,
       },
     })
-          .then((user) => user.id);
+    .then((user) => user.id);
   const user = await prisma.user.findUnique({
     where: {
       id: params.id,
-    }
+    },
   });
 
-  const {id,name,image,age,bio} = user ?? {}
+  const { id, name, image, age, bio } = user ?? {};
 
   return (
-    <main className='max-w-lg p-4 mx-auto mt-6 text-center shadow bg-slate-100'>
-         <div className='font-semibold'> User Details</div>
-    <Image className='mx-auto mt-2 rounded-sm aspect-square ' src={image}  width={80} height={80} alt={name} />
-      <h2 className='mt-2'>{name}</h2>
-      <h3>{ bio }</h3>
-      <p>{age?`${age} years` : ''} </p>
-      {currentUserId !== id  ?  <FollowButton targetUserId={id} /> : ''}
+    <main className="max-w-lg p-4 mx-auto mt-6 text-center shadow bg-slate-100">
+      <div className="font-semibold"> User Details</div>
+      <Image
+        className="mx-auto mt-2 rounded-sm aspect-square "
+        src={image}
+        width={80}
+        height={80}
+        alt={name}
+      />
+      <h2 className="mt-2">{name}</h2>
+      <h3>{bio}</h3>
+      <p>{age ? `${age} years` : ""} </p>
+      {currentUserId !== id ? <FollowButton targetUserId={id} /> : ""}
     </main>
-  )
+  );
 }
