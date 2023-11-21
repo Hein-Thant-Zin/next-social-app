7fb6988f5501352afdaab9bc119bdce7152eafab
heinthantzin <heintz414@gmail.com>
2023-11-21 22:51:38 +0630

    branch test

--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,28 @@
-export const dyanamic = 'force-static';
+export const dyanamic = "force-static";
 export const revalidate = 360;
-import AuthCheck from '@/components/AuthCheck';
-import FadeIn from '@/components/FadeIn';
-import SlideAnimation from '@/components/SlideAnimation';
-import { prisma } from '@/lib/prisma';
-import Link from 'next/link';
+import AuthCheck from "@/components/AuthCheck";
+import FadeIn from "@/components/FadeIn";
+import SlideAnimation from "@/components/SlideAnimation";
+import { prisma } from "@/lib/prisma";
+import Link from "next/link";
 // import {motion} from 'framer-motion'
 
 export async function generateStaticParams() {
-  const posts = await prisma.post.findMany();   
+  const posts = await prisma.post.findMany();
 }
 
 export default async function Home() {
   const posts = await prisma.post.findMany();
 
   return (
-    <main className='container mx-auto my-2'>
-      <section className='mx-auto'>
+    <main className="container mx-auto my-2">
+      <section className="mx-auto">
         <div className="container mx-auto text-center">
-          <FadeIn />   
-            <SlideAnimation posts={ posts } />
+          <FadeIn />
+          <SlideAnimation posts={posts} />
           <AuthCheck />
         </div>
       </section>
     </main>
-  )
+  );
 }
--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,19 +1,20 @@
-import UserCard from '@/components/UserCard';
-import { prisma } from '@/lib/prisma';
-import Image from 'next/image';
-import React from 'react'
+import UserCard from "@/components/UserCard";
+import { prisma } from "@/lib/prisma";
+import Image from "next/image";
+import React from "react";
 
 export default async function Users() {
   // throw new Error('error');
   const users = await prisma.user.findMany();
-  // console.log({users});
+
   return (
     <section>
-      <h2 className='mt-4 font-semibold text-center'>Users</h2>   
-        <div className='container flex flex-wrap mx-auto mt-2'>{users.map((user) => (
-         <UserCard user={user} key={user.id} />
+      <h2 className="mt-4 font-semibold text-center">Users</h2>
+      <div className="container flex flex-wrap mx-auto mt-2">
+        {users.map((user) => (
+          <UserCard user={user} key={user.id} />
         ))}
-        </div>
+      </div>
     </section>
-  )
+  );
 }
